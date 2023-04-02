01bc7e03a0df57c3e3548ebde5490cced7858c67
William Fischdick <jwfbit@hotmail.com>
2023-04-02 18:06:04 -0300

    :construction: feat: Criando as constantes para implementar o funcionamento da caluladora.

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+
+/* Inputs */
+
+const billNumber = document.querySelector(".bill__number");
+const customTip = document.querySelector(".custom__tip");
+const peopleNumber = document.querySelector(".people__number");
+
+
+/* Buttons */
+
+const btnTipFive = document.querySelector(".btn-tip-5");
+const btnTipTen = document.querySelector(".btn-tip-10");
+const btnTipFifteen = document.querySelector(".btn-tip-15");
+const btnTipTwentyFive = document.querySelector(".btn-tip-25");
+const btnTipFifty = document.querySelector(".btn-tip-50");
+const btnReset = document.querySelector(".btn-reset");
+
+/* Results */
+
+const tipAmountResult = document.querySelector(".tip-amount-result");
+const totalResult = document.querySelector(".total-result");
