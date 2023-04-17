1c160cc5ab025294182dcdf16a49925e3ba4c153
William Fischdick <jwfbit@hotmail.com>
2023-04-17 18:24:54 -0300

    :recycle: refactor: Diminuindo o tamanho do código para validação dos valores dos inputs.

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,61 +21,61 @@ const totalResult = document.querySelector(".total-result");
 const alertError = document.querySelector(".alert__error");
 
 
-
 btnTips.forEach((btn) => {
 
-        btn.addEventListener('click', (e) => {
-            const tipValue = e.target.innerText;
-            const billNumber = document.querySelector(".bill__number").value;
-            const peopleNumber = document.querySelector(".people__number").value;
-            
-            if (billNumber !=="" && peopleNumber !=="" && tipValue =="5%") {
-                
-                let tipPersonValue = (billNumber * 5 / 100) / peopleNumber;
-                let totalPersonValue = (billNumber / peopleNumber) + tipPersonValue;
+    btn.addEventListener('click', (e) => {
+        const tipValue = e.target.innerText;
+        const billNumber = document.querySelector(".bill__number").value;
+        const peopleNumber = document.querySelector(".people__number").value;
 
-                tipAmountResult.innerHTML = `\$ ${tipPersonValue.toFixed(2)}`;
-                totalResult.innerHTML = `\$ ${totalPersonValue.toFixed(2)}`;
+        if (billNumber !=="" && peopleNumber !=="") {
 
-            } else if (billNumber !=="" && peopleNumber !=="" && tipValue =="10%") {
-                
+            if (tipValue == "5%") {
+                    
+            let tipPersonValue = (billNumber * 5 / 100) / peopleNumber;
+            let totalPersonValue = (billNumber / peopleNumber) + tipPersonValue;
+    
+            tipAmountResult.innerHTML = `\$ ${tipPersonValue.toFixed(2)}`;
+            totalResult.innerHTML = `\$ ${totalPersonValue.toFixed(2)}`;
+
+            } else if (tipValue == "10%") {
+
                 let tipPersonValue = (billNumber * 10 / 100) / peopleNumber;
                 let totalPersonValue = (billNumber / peopleNumber) + tipPersonValue;
-
+    
                 tipAmountResult.innerHTML = `\$ ${tipPersonValue.toFixed(2)}`;
                 totalResult.innerHTML = `\$ ${totalPersonValue.toFixed(2)}`;
 
-            } else if (billNumber !=="" && peopleNumber !=="" && tipValue =="15%") {
-                
+            } else if (tipValue == "15%") {
+                    
                 let tipPersonValue = (billNumber * 15 / 100) / peopleNumber;
                 let totalPersonValue = (billNumber / peopleNumber) + tipPersonValue;
-
+    
                 tipAmountResult.innerHTML = `\$ ${tipPersonValue.toFixed(2)}`;
                 totalResult.innerHTML = `\$ ${totalPersonValue.toFixed(2)}`;
 
-            } else if (billNumber !=="" && peopleNumber !=="" && tipValue =="25%") {
-                
+            } else if (tipValue == "25%") {
+
                 let tipPersonValue = (billNumber * 25 / 100) / peopleNumber;
                 let totalPersonValue = (billNumber / peopleNumber) + tipPersonValue;
 
                 tipAmountResult.innerHTML = `\$ ${tipPersonValue.toFixed(2)}`;
                 totalResult.innerHTML = `\$ ${totalPersonValue.toFixed(2)}`;
 
-            } else if (billNumber !=="" && peopleNumber !=="" && tipValue =="50%") {
-                
+            } else if (tipValue == "50%") {
+                    
                 let tipPersonValue = (billNumber * 50 / 100) / peopleNumber;
                 let totalPersonValue = (billNumber / peopleNumber) + tipPersonValue;
-
+    
                 tipAmountResult.innerHTML = `\$ ${tipPersonValue.toFixed(2)}`;
                 totalResult.innerHTML = `\$ ${totalPersonValue.toFixed(2)}`;
-
+    
             } else {
-
+    
                 alertError.style.display = "block";
             }
-
-        })
-
+        }
+    })
 })
 
 btnReset.addEventListener('click', () => {
